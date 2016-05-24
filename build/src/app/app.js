662ec3d5ddd8668d793138a937909e6dde3ccbbe
Michal <michalittah@gmail.com>
2016-05-24 18:26:55 +0300

    removed from git ignore

---
+++ build/src/app/app.js
@@ -0,0 +1,15 @@
+angular.module( 'ngBoilerplate', [
+  'templates-app',
+  'templates-common',
+  'directive.clickedParagraph',
+  'ui.router',
+  'ui.bootstrap',
+  'modal.spell',
+  'LocalStorageModule'
+])
+
+.controller( 'AppCtrl', ["$scope", function AppCtrl ( $scope ) {
+  
+  
+}]);
+
